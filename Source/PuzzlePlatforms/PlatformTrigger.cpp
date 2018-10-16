f38ea41b0f28f4a2a1c662f545178df80d18e7fc
Flynn Ringrose <flynnringrose@gmail.com>
2018-10-16 08:55:51 -0300

    0115 - Added TArray to platform triggers so platforms now move when a player stands on the trigger.

--- Source/PuzzlePlatforms/PlatformTrigger.cpp
+++ Source/PuzzlePlatforms/PlatformTrigger.cpp
@@ -2,14 +2,19 @@
 
 #include "Components/BoxComponent.h"
 
+#include "MovingPlatform.h"
+
 APlatformTrigger::APlatformTrigger()
 {
 	PrimaryActorTick.bCanEverTick = true;
+    
     TriggerVolume = CreateDefaultSubobject<UBoxComponent>(FName("TriggerVolume"));
     if (!ensure(TriggerVolume != nullptr)) return;
     
     RootComponent = TriggerVolume;
-
+    
+    TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+    TriggerVolume->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 void APlatformTrigger::BeginPlay()
@@ -24,3 +29,18 @@ void APlatformTrigger::Tick(float DeltaTime)
 
 }
 
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+    for(AMovingPlatform* Platform : PlatformsToTrigger)
+    {
+        Platform->AddActiveTrigger();
+    }
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    for(AMovingPlatform* Platform : PlatformsToTrigger)
+    {
+        Platform->RemoveActiveTrigger();
+    }
+}
