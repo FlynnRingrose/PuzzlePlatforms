32a8f58cdc02782cffdd8d563b781234f0612d32
Flynn Ringrose <flynnringrose@gmail.com>
2018-10-16 21:18:47 -0300

    0121 - Added Exec commands.

---
+++ Source/PuzzlePlatforms/Public/PuzzlePlatformsGameInstance.h
@@ -0,0 +1,24 @@
+// Flynn's Rad Copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PuzzlePlatformsGameInstance.generated.h"
+
+UCLASS()
+class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+    
+public:
+    UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectInitializer);
+    
+    virtual void Init();
+    
+    UFUNCTION(Exec)
+    void Host();
+    
+    UFUNCTION(Exec)
+    void Join(const FString& Address);
+};
